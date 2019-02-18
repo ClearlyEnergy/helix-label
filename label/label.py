4855024adb401543dfe27a0a2f588b029c528073
ClearlyEnergy <vbugnion@clearlyenergy.com>
2019-02-18 20:05:20 +0100

    generic paths

--- label/label.py
+++ label/label.py
@@ -1,26 +1,30 @@
 from populate_residential_green_addendum import write_green_addendum_pdf
 from pdfrw import PdfWriter
 import os
+import os.path
 import boto3
 import uuid
 #lab = label.Label('filetype')
 # lab.green_addendum('./out_test.pdf', data_dict)
-INPUT_TEMPLATE_PATH = './templates/'
-OUTPUT_PATH = './tmp/'
 
+INPUT_TEMPLATE_PATH = "../templates/"
+OUTPUT_PATH = "../tmp/"
 
 class Label:
-    def __init__(self, label_name):
-        self.label_name = label_name
+    def __init__(self):
+        self.module_path = os.path.abspath(os.path.dirname(__file__))
+        self.in_path = os.path.join(self.module_path, INPUT_TEMPLATE_PATH)
+        self.out_path = os.path.join(self.module_path, OUTPUT_PATH)
         self.s3_resource = boto3.resource(
             's3',
             aws_access_key_id=os.environ.get('S3_KEY', ''),
             aws_secret_access_key=os.environ.get('S3_SECRET', '')
             )
+        print self
         
     def green_addendum(self, data_dict):
-        in_file = INPUT_TEMPLATE_PATH + 'ResidentialGreenandEnergyEfficientAddendum.pdf'
-        out_file = OUTPUT_PATH+'GreenAddendum.pdf'
+        in_file = self.in_path + 'ResidentialGreenandEnergyEfficientAddendum.pdf'
+        out_file = self.out_path +'GreenAddendum.pdf'
         write_green_addendum_pdf(in_file, data_dict, out_file)
         out_filename = self._write_S3(out_file)
         return 'https://s3.amazonaws.com/certification-label/' + out_filename
