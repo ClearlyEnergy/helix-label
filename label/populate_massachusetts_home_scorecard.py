048acfba267cf9e10560acb8b59fae87e16c6de6
Clayton <sibandaclayton0@gmail.com>
2019-11-04 09:54:57 +0200

    fixed page 2 column and added footer

--- label/populate_massachusetts_home_scorecard.py
+++ label/populate_massachusetts_home_scorecard.py
@@ -3,7 +3,7 @@
 # run with python label/populate_massachusetts_home_scorecard.py
 
 
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, BaseDocTemplate, Frame, PageTemplate, FrameBreak, NextPageTemplate
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, BaseDocTemplate, Frame, PageTemplate, FrameBreak, NextPageTemplate, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
@@ -30,6 +30,20 @@ def myLaterPages(canvas,doc):
     canvas.saveState()
     canvas.setFont('Times-Roman',9)
     canvas.drawString(inch,0.75*inch,"Page %d %s"%(doc.page,pageinfo))
+    canvas.restoreState()
+
+def footer(canvas,doc):
+    canvas.saveState()
+   
+    styles = getSampleStyleSheet()
+ 
+    # Header
+    footer = Paragraph('Home Owner | 123 Main Street , Whatley, MA 01093 Brought to you by', styles['Normal'])
+    w, h = footer.wrap(doc.width, doc.bottomMargin)
+    footer.drawOn(canvas, doc.leftMargin, h)
+    # Release the canvas
+    canvas.restoreState()   
+
 
 def go():
   
@@ -37,27 +51,28 @@ def go():
     page1_frames =[]
     page1_frameCount =3
     frameWidth = document.width/page1_frameCount
-    frameHeight = document.height-.5*inch
+    frameHeight = document.height-inch
+    bottom_margin =document.bottomMargin+0.5*inch
 
     for frame in range(page1_frameCount):
         leftMargin = document.leftMargin + frame*frameWidth
-        column =Frame(leftMargin, document.bottomMargin,frameWidth,frameHeight)
+        column =Frame(leftMargin, bottom_margin,frameWidth,frameHeight)
         page1_frames.append(column)
     
 
     page2_frames =[]
     page2_frameCount =2
-    frameWidth = document.width/page2_frameCount
+    frameWidth2 = document.width/page2_frameCount
     frameHeight = document.height-.5*inch
 
     for frame in range(page2_frameCount):
-        leftMargin = document.leftMargin + frame*frameWidth
-        column =Frame(leftMargin, document.bottomMargin,frameWidth,frameHeight)
+        leftMargin = document.leftMargin + frame*frameWidth2
+        column =Frame(leftMargin, document.bottomMargin,frameWidth2,frameHeight)
         page2_frames.append(column)
 
     templates= []
-    templates.append(PageTemplate(frames=page1_frames,id='firstPage'))
-    templates.append(PageTemplate(frames=page2_frames,id='laterPages'))
+    templates.append(PageTemplate(frames=page1_frames,id='firstPage',onPage=footer))
+    templates.append(PageTemplate(frames=page2_frames,id='secondPage',onPage=footer))
 
     document.addPageTemplates(templates)
 
@@ -67,9 +82,10 @@ def go():
     for i in range(20):
         bogustext = ("This is Paragraph number %s. " % i) *20
         p = Paragraph(bogustext, style)
-        if i==10:
+        if i==10:            
+            Story.append(NextPageTemplate('secondPage'))
             Story.append(FrameBreak())
-            Story.append(NextPageTemplate('laterPages'))
+            Story.append(Paragraph("yes",style))
         Story.append(p)
         Story.append(Spacer(1,0.2*inch))
     # print(Story)
